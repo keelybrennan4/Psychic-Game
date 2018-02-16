9c9ed9f22c0a79484b513bcb36d9e0412773d144
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-02-16 01:11:57 -0600

    fixed js so guesses display in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,47 @@
-//any letter of the alphabet may be the answer.
-var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//any letter of the alphabet may be the answer
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 //set variables for definining wins, losses and guess count, remaining guesses, and list guesses so far. 
-///var wins = 0;
-///var losses = 0;
-var guesses = document.querySelector('.guesses');
-var lastResult = document.querySelector('.lastResult');
-var guessesCount = 9;
-var guessSubmit = document.querySelector('.guessSubmit');
-var guessField = document.querySelector('.guessField');
-//var userGuess = null;
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var guessedLetters = [];
 
-var guessCount = 1;
-var resetButton;
+//computer guess
+var computerGuess = alphabet[Math.floor(Math.random()*alphabet.length)];
+console.log("Random Computer Guess: " + computerGuess)
 
-//computer picks a random letter
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+document.onkeyup = function() {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase()
+    guessedLetters.push(userGuess);
 
-function checkGuess() {
-    var userGuess = String(fromCharCode(event.keyCode).toLowerCase();
-guessSubmit.addEventListener('click', checkGuess);
-
-    if (guessCount === 1) {
-        guesses.textContent = 'Previous guesses: ';
-    }
-    guesses.textContent += userGuess + ' ';
-
+//if user wins
     if (userGuess === computerGuess) {
-        last.Result.textContent = 'Congratulations! You are a psychic!';
-        setGameOver();
-    }else if (guessCount === 0) {
-        lastResult.textContent = 'You Lost! Try Again?'
-        setGameOver();
+        wins++;
+        alert("You Win!");
+        resetGame()
+//if user guesses incorrectly, and still has guesses remaining
+    } else {
+        guesses--;
     }
-    guessCount++;
-    guessField.value = '';
-    guessField.focus();
-}
-
-//when game is over
-function setGameOver() {
-    guessField.disabled = true;
-    guessSubmit.disabled = true;
-    resetButton = document.createElement('button');
-    resetButton.textContent = 'Start new game';
-    resetButton.addEventListener('click', resetGame);
-}
-
-//to play again
-function resetGame() {
-    guessCount = 9;
-    var resetParas = document.querySelectorAll('resultParas p');
-    for (var i = 0 ; i < resetParas.length ; i++){
-        resetParas[i].textContent = '';
+//if user is out of guesses, she loses
+    if (guesses===0) {
+        losses++;
+        alert("You lose!");
+        resetGame()
     }
-    guessField.value = '';
-    guessField.focus();
-    guessField.disabled = false;
-    guessSubmit.disabled = false;
-   
-    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+//function to reset the game 
+    function resetGame() {
+        guesses = 10;
+        guessedLetters = [];
+        computerGuesses = alphabet[Math.floor(Math.random()*alphabet.length)];
+        console.log("Letter I'm thinking of: " + computerGuess);
+    }
+//update html counts 
+    var gameHtml = "Guess what letter I'm thinking of!" +
+        "<p>Wins: " + wins + "</p>"+
+        "<p>Losses: " + losses + "</p>" +
+        "Guesses Remaining: " + guesses + "</p>" +
+        "Guessed Letters: " + guessedLetters.join(',') + "</p>" ;
+        document.getElementById("game-html").innerHTML = gameHtml;
 }
