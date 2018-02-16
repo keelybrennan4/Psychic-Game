74c28924844712df5c60a50b85065bdb124f3738
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-02-16 00:10:13 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,63 @@
-//define variables. any letter of the alphabet may be the answer.
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
-    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-//set initial global variables for definining wins, losses and guess count.  
-var wins = 0;
-var losses = 0;
-var guessesRemaining = 10;
-//will hold all the users guesses in each round
-var guessesSoFar = [];
-//userGuess is what the letter the user picks by pressing a key
-var userGuess = null;
-//computer picks a random guess and stores in computerChoice 
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log("Wins: " + wins + " Losses: " + losses +  "Guesses Remaining: " + guessesRemaining + "Guesses so far: " + guessesSoFar + " Computer Choice: " + computerChoice);
+//any letter of the alphabet may be the answer.
+var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+//set variables for definining wins, losses and guess count, remaining guesses, and list guesses so far. 
+///var wins = 0;
+///var losses = 0;
+var guesses = document.querySelector('.guesses');
+var lastResult = document.querySelector('.lastResult');
+var guessesCount = 9;
+var guessSubmit = document.querySelector('.guessSubmit');
+var guessField = document.querySelector('.guessField');
+//var userGuess = null;
+
 var guessCount = 1;
 var resetButton;
 
-//When user presses a key, record and save to userGuess then run function to check if guess is correct. 
-document.onkeypress = function(checkGuess) {
+//computer picks a random letter
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    // When user presses a key, it records it and saves to userGuess
-    var userGuess = checkGuess.key;
+function checkGuess() {
+    var userGuess = String(fromCharCode(event.keyCode).toLowerCase();
+guessSubmit.addEventListener('click', checkGuess);
 
-    // Add user's guess to the guessSoFar array. Make sure the user's pick is within the alphabet variables 
-    if (guessesSoFar.indexOf(userGuess) < 0 && alphabet.indexOf(userGuess) >= 0) {
-        guessesSoFar.push[userGuess];
-        // if it is a new letter then decrease remaining guesses by 1
-		guessesRemaining--;
+    if (guessCount === 1) {
+        guesses.textContent = 'Previous guesses: ';
     }
+    guesses.textContent += userGuess + ' ';
 
-    //If correct, user wins. If incorrect, subtract one from guesses remaining value.    
-    if (userGuess == computerChoice){
-        wins++
-        console.log(Woohoo!);
-        guesses
-        document.getElementById('wins').innerHTML = "Wins: " + wins;
-        
-    }else{
-        guessesRemaining--;
+    if (userGuess === computerGuess) {
+        last.Result.textContent = 'Congratulations! You are a psychic!';
+        setGameOver();
+    }else if (guessCount === 0) {
+        lastResult.textContent = 'You Lost! Try Again?'
+        setGameOver();
     }
-//If user loses, add one to user's loss count. 
-    if(guessCount === 0){
-        losses++
-    }
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
 }
-document.getElementById('wins').innerHTML = "Wins: " + wins;
-document.getElementById('losses').innerHTML = "Losses: " + losses;
-document.getElementById('guessesRemaining').innerHTML = "Guesses Remaining: " + guessesRemaining;
+
+//when game is over
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    resetButton.addEventListener('click', resetGame);
+}
+
+//to play again
+function resetGame() {
+    guessCount = 9;
+    var resetParas = document.querySelectorAll('resultParas p');
+    for (var i = 0 ; i < resetParas.length ; i++){
+        resetParas[i].textContent = '';
+    }
+    guessField.value = '';
+    guessField.focus();
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+   
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
